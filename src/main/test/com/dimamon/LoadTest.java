658c2c5ba19f456ca5ddcf5a80907edef5c31406
Dmitriy Gutman <dimamon@list.ru>
2018-11-04 14:12:16 +0200

    cpu load test added

---
+++ src/main/test/com/dimamon/LoadTest.java
@@ -0,0 +1,21 @@
+package com.dimamon;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+class LoadTest {
+
+    @Test
+    void loadTest() throws IOException, InterruptedException {
+        for (int i = 0; i < 10000 ; i++) {
+            Thread.sleep(5);
+            final String url = "http://localhost:8080/students/2";
+            URLConnection connection = new URL(url).openConnection();
+            InputStream inputStream = connection.getInputStream();
+        }
+    }
+}
