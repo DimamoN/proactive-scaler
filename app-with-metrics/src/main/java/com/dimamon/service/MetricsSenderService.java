27de9da378ad55156df848d066c79cf8ad5b54a6
dimamon <dimamon95@gmail.com>
2018-11-24 21:50:18 +0200

    kubernetes support added

--- app-with-metrics/src/main/java/com/dimamon/service/MetricsSenderService.java
+++ app-with-metrics/src/main/java/com/dimamon/service/MetricsSenderService.java
@@ -4,8 +4,11 @@ import com.dimamon.repo.MeasurementsRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 import static com.dimamon.utils.StringUtils.stringify;
 
@@ -17,11 +20,28 @@ public class MetricsSenderService {
     private static final int INITIAL_DELAY = 10 * 1000;
     private static final int SEND_LOAD_EVERY = 3 * 1000;
 
+    @Value("${kubernetes.node_name}")
+    private String nodeName;
+
+    @Value("${kubernetes.pod_name}")
+    private String podName;
+
     @Autowired
     private ResourcesService resourcesService;
 
     @Autowired
     private MeasurementsRepo measurementsRepo;
+
+    @PostConstruct
+    private void init() {
+        if (nodeName == null) {
+            nodeName = "localhost";
+        }
+        if (podName == null) {
+            podName = "app";
+        }
+        LOGGER.info("NODE={}, POD={}", nodeName, podName);
+    }
 
     @Scheduled(initialDelay = INITIAL_DELAY, fixedDelay = SEND_LOAD_EVERY)
     public void sendMetrics() {
@@ -30,7 +50,8 @@ public class MetricsSenderService {
         long freeMemory = resourcesService.getFreeMemory();
         long totalMemory = resourcesService.getTotalMemory();
 
-        LOGGER.info("# # # #");
+        LOGGER.info("# # # # {} / {} ", nodeName, podName);
+
         LOGGER.info("OS STATS  # cpuLoad={} processLoad={} free_ram={} total_ram={}",
                 stringify(cpuLoad), stringify(cpuLoadProcess), freeMemory, totalMemory);
 
@@ -42,8 +63,8 @@ public class MetricsSenderService {
         LOGGER.info("JVM STATS # free/total/max jvm_ram=[{}/{}/{}]",
                 freeJVMMem, totalJVMMem, maxJVMMem);
 
-        measurementsRepo.measureLoad("app1", cpuLoad, freeMemory, totalMemory);
-        measurementsRepo.measureJVMLoad("app1", freeJVMMem, totalJVMMem, maxJVMMem);
+        measurementsRepo.measureLoad(podName, cpuLoad, freeMemory, totalMemory);
+        measurementsRepo.measureJVMLoad(podName, freeJVMMem, totalJVMMem, maxJVMMem);
     }
 
 }
