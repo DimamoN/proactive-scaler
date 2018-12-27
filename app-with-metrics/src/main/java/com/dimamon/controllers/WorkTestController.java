229ec1572360dc76b8a064114b033125eb885801
dimamon <dimamon95@gmail.com>
2018-12-27 22:56:43 +0200

    configs tuned

--- app-with-metrics/src/main/java/com/dimamon/controllers/WorkTestController.java
+++ app-with-metrics/src/main/java/com/dimamon/controllers/WorkTestController.java
@@ -3,6 +3,7 @@ package com.dimamon.controllers;
 import com.dimamon.repo.MeasurementsRepo;
 import com.dimamon.service.WorkloadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,21 +20,24 @@ public class WorkTestController {
     private WorkloadService workloadService;
 
     @RequestMapping(value = "/cpu/{power}", method = RequestMethod.GET)
-    public void cpuPower(@PathVariable("power") Integer power) {
+    public String cpuPower(@PathVariable("power") Integer power) {
         measurementsRepo.measureConnection(power, "cpu");
         workloadService.loadCpu(power);
+        return HttpStatus.OK.toString();
     }
 
     @RequestMapping(value = "/ram/{power}", method = RequestMethod.GET)
-    public void ramPower(@PathVariable("power") Integer power) {
+    public String ramPower(@PathVariable("power") Integer power) {
         measurementsRepo.measureConnection(power, "ram");
         workloadService.loadRam(power);
+        return HttpStatus.OK.toString();
     }
 
     @RequestMapping(value = "/ram/clear", method = RequestMethod.GET)
-    public void ramPower() {
+    public String ramPower() {
         measurementsRepo.measureConnection(1, "ram_clear");
         workloadService.clearRam();
+        return HttpStatus.OK.toString();
     }
 
 }
